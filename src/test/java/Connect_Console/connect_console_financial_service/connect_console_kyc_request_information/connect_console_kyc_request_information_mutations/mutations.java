c8daf187be49d40f006d01345b9d0155d64ab70c
Mehedi Hasan <meheedihasan01@gmail.com>
2023-07-18 12:54:50 +0600

    Added two new methods Statement Request and StatementBulk Request

--- src/test/java/Connect_Console/connect_console_financial_service/connect_console_kyc_request_information/connect_console_kyc_request_information_mutations/mutations.java
+++ src/test/java/Connect_Console/connect_console_financial_service/connect_console_kyc_request_information/connect_console_kyc_request_information_mutations/mutations.java
@@ -9,15 +9,27 @@ import java.io.IOException;
 
 public class mutations {
     @Test(description = "Check if Kyc_reqest_no will contain exactly 10 characters long, consists of alphanumeric capital letters, and does not contain any special characters")
-    public void KycRequest() throws IOException, ConfigurationException {
+    public void MutationKycRequest() throws IOException, ConfigurationException {
         TestCase user=new TestCase();
         user.mutationKycRequest();
     }
 
-    @Test(description = "Check if File has been successfully uploaded")
-    public void KycBulkRequest() throws IOException, ConfigurationException {
+    @Test(description = "Kyc Bulk: Check if File has been successfully uploaded")
+    public void MutationKycBulkRequest() throws IOException, ConfigurationException {
         TestCase user=new TestCase();
         user.mutationKycBulkRequest();
+    }
+
+    @Test(description = "Check if statement_request_no will contain exactly 10 characters long, consists of alphanumeric capital letters, and does not contain any special characters")
+    public void MutationStatementRequest() throws IOException, ConfigurationException {
+        TestCase user=new TestCase();
+        user.mutationStatementRequest();
+    }
+
+    @Test(description = "Statement Bulk: Check if File has been successfully uploaded")
+    public void MutationStatementBulkRequest() throws IOException, ConfigurationException {
+        TestCase user=new TestCase();
+        user.mutationStatementBulkRequest();
     }
 
 }
